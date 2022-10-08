733d4d5a159e075bbdc74c6f3c3074d68dc696a4
GD9918 <115349226+GD9918@users.noreply.github.com>
2022-10-09 00:46:40 +0900

    Update list.java

--- list.java
+++ list.java
@@ -1,7 +1,7 @@
 package pro1;
 import java.util.Scanner;
 import java.util.LinkedList;
-
+//20191093 박건도
 //ET = Element Type
 
 //LinearList 인터페이스
@@ -57,20 +57,22 @@ class SLinkedList <ET> implements LinearList<ET>{
 		}
 		
 		if(cn == null) 
-			return false;  // not found
+			return false;  // 지우려는 값이 리스트에 없어서 찾지못할떄.
 		
-		if(cn.equals(first))
+		if(cn.equals(first)) //if 지우려는 값(cn)이 첫번째(first)값일때.
 		{
-			Node<ET> NextNode = first.next;
-			first=NextNode;
+			Node<ET> NextNode = first.next; 
+			first=NextNode; //cn의 이전값이 없기때문에 다음값의 주소값과 같다고 설정. ??
 			size --;
 			return true;
 		}
 		
 		pn.next = cn.next;
 		size--;
-		return true;
-	}
+		return true; 
+		//위처럼 return값이 있을때 main에서 출력하려면 print문을 사용하여야한다.
+		//만약 return값이 없고 void ~~ 이면 print없이 함수를 호출하면 된다. 
+		}
 	
 	//LinearList 인터페이스 속에 있는 search 함수 오버라이드
 	@Override
@@ -89,11 +91,11 @@ class SLinkedList <ET> implements LinearList<ET>{
 	@Override
 	public ET get(int index) {
 		if (index < 0 || index >= size) { 
-			try {
+			try { // ***내가 찾으려는 elem이 생성되어 있는 리스트에 없을때 
 				throw new IndexOutOfBoundsException();
 			}
 			catch(IndexOutOfBoundsException e){
-				return null;
+				return null;  // ***null값을 반환한다. 
 			}
 		}
 		Node<ET> cn = first;
@@ -105,6 +107,7 @@ class SLinkedList <ET> implements LinearList<ET>{
 	
 	//LinearList 인터페이스 속에 있는 show 함수 오버라이드
 	@Override
+	//show()는 void. 즉 return값이 없으므로 main에서 출력할  때 list.show()하면 print문 없이 출력 가능.
 	public void show() {
 		Node<ET> cn = first;
 		for(int i=0; i<size; ++i) {
@@ -120,26 +123,34 @@ class SLinkedList <ET> implements LinearList<ET>{
 //Main문
 public class test11 {
 	public static void main(String[] args) {
-		SLinkedList<Integer> sl = new SLinkedList<Integer>();
-		sl.insert(50);
-		sl.insert(30);
-		sl.insert(70);
-		sl.insert(20);
-		sl.insert(40);
-		sl.insert(60);
-		sl.show();	
-		System.out.println(sl.get(3));
-		System.out.println(sl.get(9));
+		SLinkedList<Integer> list = new SLinkedList<Integer>();
+		//list 생성후 값 삽입. 
+		list.insert(50);
+		list.insert(30);
+		list.insert(70);
+		list.insert(20);
+		list.insert(40);
+		list.insert(60);
 		
-		System.out.println(sl.search(30));
-		System.out.println(sl.search(90));
+		//show()호출 하여 list값 전체 출력. 
+		list.show();	
 		
-		sl.remove(60);
-		sl.remove(70);
-		sl.remove(50);
-		sl.remove(90);
+		//return값이 있는 get함수는 print사용. get함수는 원하는 순서에 있는 elem 출력.
+		System.out.println(list.get(3));
+		System.out.println(list.get(9));
 		
-		sl.show();
+		//search는 내가 원하는elem이 리스트 몇번째에 존재하는지 출력.
+		System.out.println(list.search(30));
+		System.out.println(list.search(90));
+		
+		//리스트에서 삭제하고 싶은 elem을 입력하면 리스트에서 삭제.
+		list.remove(60);
+		list.remove(70);
+		list.remove(50);
+		list.remove(90);
+		
+		//삭제되고 난 뒤의 리스트 전체값을 출력.
+		list.show();
 		
 		
 		
